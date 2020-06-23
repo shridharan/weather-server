2b4f2dcf090ef5d6008cebc553cb66bd7e595009
shridharan <shridharan.m@gmail.com>
2020-06-22 18:12:13 -0700

    setup heroku stuff

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { response } = require('express')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDir = __dirname+'/../public'
 console.log(publicDir)
 app.use(express.static(publicDir))
@@ -10,6 +12,6 @@ app.get('/weather', () => {
     res.send('Weather is sunny')
 })
 
-app.listen(3000, () => {
-    console.log("Weather server starting at port 3000")
+app.listen(port, () => {
+    console.log(`Weather server starting at port  ${port}`)
 })
